58f7f3fd2e263da7ceec0c90f99a8ea0f04fb3f7
ishween <ishweensehmbhi@gmail.com>
2022-02-15 16:00:52 -0500

    display API data on page and add select element

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ artApp.apiUrl = "https://www.rijksmuseum.nl/api/en/collection";
 
 // make call to the API, get some data back
 // take the data and put it on the page
-artApp.getArt = function () {
+artApp.getArt = function (userChoice) {
 	// format API endpoint
 	const url = new URL(artApp.apiUrl);
 
 	// format and add params to url
 	url.search = new URLSearchParams({
 		key: artApp.apiKey,
-		q: "monkey",
+		q: userChoice,
 		imgonly: true,
 	});
 
@@ -27,16 +27,63 @@ artApp.getArt = function () {
 			return res.json();
 		})
 		.then(function (jsonRes) {
-			console.log(jsonRes.artObjects);
+			// display the art
+			artApp.displayArt(jsonRes.artObjects);
 		});
 };
 
-artApp.displayArt = (jsonRes) => {};
+// create a method which will take the API data and display it on our page
+artApp.displayArt = function (artArray) {
+	// iterate through each object and get relevant info, construct an element
+	artArray.forEach(function (artwork) {
+		// console.log(artwork);
+
+		// extract important data from API, save within variables
+		const artworkTitle = artwork.title;
+		const artworkImage = artwork.webImage.url;
+		const artist = artwork.principalOrFirstMaker;
+		const altText = artwork.longTitle;
+
+		// create an li element with a class of piece in which this info will be added
+		const listElement = document.createElement("li");
+		listElement.classList.add("piece");
+
+		// create an h2 to hold the art title
+		const heading = document.createElement("h2");
+		heading.textContent = artworkTitle;
+
+		// create an img to hold the artwork picture
+
+		const image = document.createElement("img");
+
+		// element node has src and alt properties which we can use
+		image.alt = altText;
+		image.src = artworkImage;
+
+		// create a p  with a class of artist to hold artist name
+		const paragraphElement = document.createElement("p");
+		paragraphElement.classList.add("artist");
+		paragraphElement.textContent = artist;
+
+		// take the elements we have created and add them to the li
+		listElement.appendChild(heading);
+		listElement.appendChild(image);
+		listElement.appendChild(paragraphElement);
+
+		// add the li to the ul so that the data is finally in the DOM
+		const ulElement = document.getElementById("artwork");
+		ulElement.appendChild(listElement);
+
+		// console.log(artworkTitle, artworkImage, artist, altText);
+	});
+};
+
+artApp.getUserChoice = function () {};
 
 // create an init method
 artApp.init = function () {
-	console.log("App is initialized");
-	artApp.getArt();
+	console.log("App is initialized.");
+	artApp.getArt("porcelain");
 };
 
 // Call the init method
